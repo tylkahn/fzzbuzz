495137d3da56572879311e4db414c290b3032fcd
tylkahn <tylkahn@nuevaschool.org>
2017-03-15 20:56:34 -0700

    Create fzzbuzz.sh

---
+++ fzzbuzz.sh
@@ -0,0 +1,17 @@
+read -s var
+
+x=1
+while ((x < var+1));
+        do
+
+if (! ((x % 3)) ) && (! ((x % 5)) ); then echo "FizzBuzz";
+    elif (! ((x % 3)) ); then echo "Fizz";
+    elif (! ((x % 5)) ); then echo "Buzz";
+
+else
+        echo $x;
+
+fi
+(( x++ ))
+
+done
